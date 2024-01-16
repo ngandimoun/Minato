2f88fc6a8e0dfa7e028cdf5f278c81793970ecf1
useminatoai <154894649+useminatoai@users.noreply.github.com>
2024-01-16 16:47:12 +0000

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,15 @@ import git
 
 import re
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2B8MCPB9HD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2B8MCPB9HD');
+</script>
 
 st.set_page_config(layout="wide")
 

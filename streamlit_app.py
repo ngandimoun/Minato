fd2e689937ea7a9b8cc32b0a79672d400e1d3dcf
useminatoai <154894649+useminatoai@users.noreply.github.com>
2024-01-17 21:42:47 +0000

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -370,7 +370,7 @@ def main():
     st.markdown("\n")
     st.markdown("\n")
     st.markdown("\n")
-    st.markdown("### Examine the Names of Directories and Files.")
+    # st.markdown("### Examine the Names of Directories and Files.")
     def file_explorer(path):
         files, dirs = list_files_and_dirs(path)
 

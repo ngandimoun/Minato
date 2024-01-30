14f21253f29700cb3cf5b795a293ab82c8f9a9d7
Chris NGANDIMOUN <43313325+ngandimoun@users.noreply.github.com>
2024-01-30 23:52:51 +0000

    Update softwareaasistant.py

--- softwareaasistant.py
+++ softwareaasistant.py
@@ -416,7 +416,7 @@ if prompt := st.chat_input("Ask Minato, your Software Questions??"):
 
 
 # Save chat history after each interaction
-save_chat_history(st.session_state.messages)
+#save_chat_history(st.session_state.messages)
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """

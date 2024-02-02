5e591da33ec2f94ac39cad4a74cdb68e00ee7b56
Chris NGANDIMOUN <43313325+ngandimoun@users.noreply.github.com>
2024-02-02 00:59:16 +0000

    Update realWordUse.py

--- realWordUse.py
+++ realWordUse.py
@@ -1005,12 +1005,3 @@ if __name__ == "__main__":
 
 
 
-# ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)

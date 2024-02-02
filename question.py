8fb5bdc8c4042053b8b0d1dd4f552f7f2de5a60f
Chris NGANDIMOUN <43313325+ngandimoun@users.noreply.github.com>
2024-02-02 00:59:44 +0000

    Update question.py

--- question.py
+++ question.py
@@ -360,13 +360,3 @@ if __name__ == "__main__":
     main()
     
     
-
-# ---- HIDE STREAMLIT STYLE ----
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)

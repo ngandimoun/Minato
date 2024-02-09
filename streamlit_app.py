d20babf62a6019e7039690640943c226f748cae2
Chris NGANDIMOUN <43313325+ngandimoun@users.noreply.github.com>
2024-02-09 23:48:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,6 +94,26 @@ st.set_page_config(layout="wide")
 st.markdown(sidebar_style, unsafe_allow_html=True)
 
 
+
+# Google Tag Manager code
+gtm_code = """
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N4W5J4DN');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N4W5J4DN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+"""
+
+# Injecting Google Tag Manager code into the HTML head
+st.markdown(gtm_code, unsafe_allow_html=True)
+
 # Sidebar for navigation
 st.sidebar.title('🧭 Navigation')
 options = st.sidebar.radio('Go to', ['🏠 Home', '🛠️ Software Assistant', '🐍 Python Problems', '🌐 Real World Case'])
